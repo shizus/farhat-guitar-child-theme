d8a011cac00f6fb24a3fe82b2c885410c73e8998
Gabriel La Torre <latorregab@gmail.com>
2026-08-01 16:42:28 -0300

    chore: add package.json and update dependencies to fix security vulnerabilities

--- farhat-gutiar-divi-child/assets/src/gulpfile.js
+++ farhat-gutiar-divi-child/assets/src/gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
+var sass = require('gulp-sass')(require('sass'));
 const minify = require('gulp-minify');
 var concat = require('gulp-concat');
 
